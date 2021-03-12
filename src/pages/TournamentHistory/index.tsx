b738486c141f44b3d57ba3521cd218e101b61a2e
Aluminaze <aluminaze@gmail.com>
2021-03-13 00:29:42 +0300

    add empty pages: - Tournament - Tournament history - Tournament rating

---
+++ src/pages/TournamentHistory/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TournamentHistory = () => {
+  return (
+    <>
+      <span>History</span>
+    </>
+  );
+};
+
+export default TournamentHistory;
---
+++ src/pages/TournamentRating/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TournamentRating = () => {
+  return (
+    <>
+      <span>Rating</span>
+    </>
+  );
+};
+
+export default TournamentRating;
