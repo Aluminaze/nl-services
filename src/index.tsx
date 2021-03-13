261628fc5869ce9fa071ab4fd3b175ba9e54c734
Aluminaze <aluminaze@gmail.com>
2021-03-14 01:40:10 +0300

    use firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import firebase from "firebase";
+import firebaseConfig from "firebaseConfig";
 
 const theme = createMuiTheme({
   palette: {
@@ -17,17 +18,6 @@ const theme = createMuiTheme({
     },
   },
 });
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
 
 firebase.initializeApp(firebaseConfig);
 
