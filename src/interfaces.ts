bd573bc74da073675aef831ff122f078ded968b1
Aluminaze <aluminaze@gmail.com>
2021-03-15 00:04:19 +0300

    move interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,3 +4,27 @@ export interface ContextProps {
   auth: firebase.auth.Auth;
   database: firebase.database.Database;
 }
+
+export interface ParticipantInfoStruct {
+  id: string;
+  count: number;
+}
+export interface ParticipantsStruct {
+  [key: string]: ParticipantInfoStruct;
+}
+export interface TournamentInTimeStruct {
+  winner: string;
+  participiants: ParticipantsStruct;
+}
+
+export interface TournamentStruct {
+  id: string;
+  time11: TournamentInTimeStruct;
+  time15: TournamentInTimeStruct;
+  time19: TournamentInTimeStruct;
+  time23: TournamentInTimeStruct;
+}
+
+export interface RenderParticipantsProps {
+  participiants: ParticipantsStruct;
+}
