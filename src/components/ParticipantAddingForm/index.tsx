2a501f0d04b2711a2d59cfeae22fbe7ba468f162
Aluminaze <aluminaze@gmail.com>
2021-03-14 23:19:25 +0300

    add component for adding participant

---
+++ src/components/ParticipantAddingForm/index.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import useStyles from "./styles";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { Button } from "@material-ui/core";
+import _ from "lodash";
+
+interface ParticipantAddingFormProps {
+  setIsAdding: (status: boolean) => void;
+}
+const participants: string[] = ["*Ricardo*", "Muhamed_mc", "ПРОВАР", "~A~I~D~"];
+const amount: string[] = _.range(1, 17).map((num) => String(num));
+
+const ParticipantAddingForm = (props: ParticipantAddingFormProps) => {
+  const { setIsAdding } = props;
+  const classes = useStyles();
+  const [selectedParticipant, setSelectedParticipant] = useState<string | null>(
+    null
+  );
+  const [inputParcipName, setInputParcipName] = useState<string>("");
+  const [inputAmountOfMeat, setInputAmountOfMeat] = useState<string | null>(
+    null
+  );
+  const [inputAmount, setInputAmount] = useState<string>("");
+
+  return (
+    <>
+      <h2 className={classes.title}>Заполните поля</h2>
+
+      <div className={classes.inputWrapper}>
+        <Autocomplete
+          value={selectedParticipant}
+          size="small"
+          onChange={(event: any, newValue: string | null) => {
+            setSelectedParticipant(newValue);
+          }}
+          inputValue={inputParcipName}
+          onInputChange={(event, newInputValue) => {
+            setInputParcipName(newInputValue);
+          }}
+          id="participant-autocomplete"
+          options={participants}
+          style={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Выберите персонажа"
+              variant="outlined"
+            />
+          )}
+        />
+      </div>
+
+      <div className={classes.inputWrapper}>
+        <Autocomplete
+          value={inputAmountOfMeat}
+          size="small"
+          onChange={(event: any, newValue: string | null) => {
+            setInputAmountOfMeat(newValue);
+          }}
+          inputValue={inputAmount}
+          onInputChange={(event, newInputValue) => {
+            setInputAmount(newInputValue);
+          }}
+          id="amount-of-meat-autocomplete"
+          options={amount}
+          style={{ width: 170 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Привел мяска" variant="outlined" />
+          )}
+        />
+      </div>
+
+      <div className={classes.buttonWrapper}>
+        <Button
+          variant="contained"
+          size="small"
+          color="primary"
+          onClick={() => setIsAdding(false)}
+          disabled={inputParcipName && inputAmountOfMeat ? false : true}
+        >
+          Сохранить
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default ParticipantAddingForm;
---
+++ src/components/ParticipantAddingForm/styles.ts
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    fontSize: 16,
+  },
+  inputWrapper: {
+    margin: "15px 0 0 0",
+  },
+  buttonWrapper: {
+    margin: "10px 0 0 0",
+  },
+}));
+
+export default useStyles;
