7a3c432e883f490d4b625a9cd8d4682a8d1dc3fd
Aluminaze <aluminaze@gmail.com>
2021-03-16 01:41:05 +0300

    implement users rating

--- src/pages/TournamentRating/index.tsx
+++ src/pages/TournamentRating/index.tsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "index";
+import { UserStruct } from "interfacesAndTypes";
+import { useObjectVal } from "react-firebase-hooks/database";
+import useStyles from "./styles";
+import clsx from "clsx";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const TournamentRating = () => {
+  const classes = useStyles();
+  const { database } = useContext(Context);
+
+  // firebase refs
+  const refRatingUsers = database.ref("users");
+
+  // firebase data
+  const [usersValData, loading] = useObjectVal<{ [key: string]: UserStruct }>(
+    refRatingUsers
+  );
+  const usersData: UserStruct[] = usersValData
+    ? Object.values(usersValData)
+    : [];
+
   return (
-    <>
-      <span>Rating</span>
-    </>
+    <section
+      className={clsx(classes.container, loading ? classes.alignCenter : null)}
+    >
+      {loading ? (
+        <CircularProgress color="primary" />
+      ) : (
+        <div className={classes.tableWrapper}>
+          <ul className={classes.tableContainer}>
+            {usersData.length
+              ? usersData.map((userData: UserStruct, index: number) => (
+                  <li
+                    className={clsx(
+                      classes.tableRow,
+                      index % 2 === 0 ? classes.tableRowWithBackground : null
+                    )}
+                    key={index}
+                  >
+                    <span className={classes.userName}>{userData.name}</span>
+                    <span className={classes.userScore}>{userData.score}</span>
+                  </li>
+                ))
+              : null}
+          </ul>
+        </div>
+      )}
+    </section>
   );
 };
 
