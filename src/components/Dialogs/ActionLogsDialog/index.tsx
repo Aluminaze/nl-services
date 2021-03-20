43baae15c7463792d966875d56efa157addcabab
Aluminaze <aluminaze@gmail.com>
2021-03-20 21:16:22 +0300

    delete unused imports

--- src/components/Dialogs/ActionLogsDialog/index.tsx
+++ src/components/Dialogs/ActionLogsDialog/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import useStyles from "./styles";
 import getTimeByTimeKey from "utils/getTimeByTimeKey";
@@ -12,8 +10,6 @@ import {
   WithStyles,
 } from "@material-ui/core/styles";
 import MuiDialogTitle from "@material-ui/core/DialogTitle";
-import MuiDialogContent from "@material-ui/core/DialogContent";
-import MuiDialogActions from "@material-ui/core/DialogActions";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import Typography from "@material-ui/core/Typography";
