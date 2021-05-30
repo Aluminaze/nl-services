da17e598e19655f46068e18ef6d6b7dd7174c6b3
Aluminaze <aluminaze@gmail.com>
2021-05-31 01:40:30 +0300

    fix start page layout for mobile devices

--- src/pages/LogIn/style.ts
+++ src/pages/LogIn/style.ts
@@ -1,13 +1,17 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Theme } from "@material-ui/core";
 
-const useStyles = makeStyles(() => {
+const useStyles = makeStyles((theme: Theme) => {
   return {
     main: {
-      gridArea: "main",
+      gridArea: "container",
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
       width: "100%",
+
+      [theme.breakpoints.down("sm")]: {
+        padding: "10px",
+      },
     },
     content: {
       display: "flex",
@@ -17,14 +21,19 @@ const useStyles = makeStyles(() => {
       height: "100%",
     },
     authBlock: {
-      width: 600,
-      height: 350,
+      minWidth: 600,
+      minHeight: 350,
       padding: "20px 30px",
       display: "flex",
       flexDirection: "column",
       justifyContent: "center",
       border: "1px solid rgba(151,151,151, .5)",
       borderRadius: "15px",
+
+      [theme.breakpoints.down("sm")]: {
+        minWidth: "auto",
+        minHeight: "auto",
+      },
     },
     authBlockHeader: {
       display: "flex",
@@ -34,6 +43,12 @@ const useStyles = makeStyles(() => {
 
       "& h1": {
         fontSize: 20,
+      },
+
+      [theme.breakpoints.down("sm")]: {
+        "& h1": {
+          fontSize: "1.2rem",
+        },
       },
     },
   };
