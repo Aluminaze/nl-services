d085690eb0d88639a2c46a1e32dea3f0e3a3e3df
Aluminaze <aluminaze@gmail.com>
2021-07-02 00:02:09 +0300

    update styles

--- src/components/TournamentsNavigation/styles.ts
+++ src/components/TournamentsNavigation/styles.ts
@@ -1,4 +1,6 @@
 import { makeStyles, Theme } from "@material-ui/core";
+import { HEADER_HEIGHT } from "utils/constants";
+
 const useStyles = makeStyles((theme: Theme) => ({
   nav: {
     gridArea: "nav",
@@ -10,7 +12,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   navButtons: {
     position: "sticky",
-    top: 40,
+    top: HEADER_HEIGHT,
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-between",
