8968aaf9f39562f2afc1b8668ddc21aeca641480
Aluminaze <aluminaze@gmail.com>
2021-08-28 20:39:56 +0300

    update logic of component

--- src/pages/TournamentHistory/index.tsx
+++ src/pages/TournamentHistory/index.tsx
@@ -1,32 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { useList } from "react-firebase-hooks/database";
 import useStyles from "./styles";
-import { TournamentStruct } from "interfacesAndTypes";
-import TournamentAtTime from "components/TournamentAtTime";
-import {
-  TIME_KEY_11,
-  TIME_KEY_15,
-  TIME_KEY_19,
-  TIME_KEY_23,
-} from "utils/constants";
-import firebase from "firebase";
 import TournamentDatePicker from "components/TournamentDatePicker";
+import { useHistory } from "react-router";
 
 const TournamentHistory = (): JSX.Element => {
   const classes = useStyles();
+  const history = useHistory();
 
-  const database = firebase.database();
   const [tournamentFullDate, setTournamentFullDate] = useState<Date | null>(
     null
-  );
-  const [selectedDate, setSelectedDate] = useState<string>("");
-
-  // firebase refs
-  const refTournaments = database.ref("tournaments");
-
-  // firenase data
-  const [tournamentsAtDay, loadingTournamentsAtDay] = useList(
-    refTournaments.orderByChild("id").equalTo(selectedDate)
   );
 
   useEffect(() => {
@@ -34,13 +16,11 @@ const TournamentHistory = (): JSX.Element => {
       const date: number = tournamentFullDate.getDate();
       const mounth: number = tournamentFullDate.getMonth() + 1;
       const year: number = tournamentFullDate.getFullYear();
-      const pickedDate: string = `${date}/${mounth}/${year}`;
+      const pickedDate: string = `${date}-${mounth}-${year}`;
 
-      setSelectedDate(pickedDate);
-    } else {
-      setSelectedDate("");
+      history.push(`/history/${pickedDate}`);
     }
-  }, [tournamentFullDate]);
+  }, [history, tournamentFullDate]);
 
   const handleDateChange = (date: Date | null) => {
     setTournamentFullDate(date);
@@ -52,51 +32,6 @@ const TournamentHistory = (): JSX.Element => {
         date={tournamentFullDate}
         onChange={handleDateChange}
       />
-
-      {selectedDate && !loadingTournamentsAtDay && (
-        <div className={classes.table}>
-          {tournamentsAtDay?.length ? (
-            tournamentsAtDay.map((tournamentSnapshot, index: number) => {
-              const tournamentData: TournamentStruct = tournamentSnapshot.val();
-
-              return (
-                <div className={classes.tableWrapper} key={index}>
-                  <div className={classes.tableContainer}>
-                    <TournamentAtTime
-                      tournamentDateId={selectedDate}
-                      timeKey={TIME_KEY_11}
-                      tournamentSnapshot={tournamentSnapshot}
-                      participants={tournamentData.time11?.participants}
-                    />
-                    <TournamentAtTime
-                      tournamentDateId={selectedDate}
-                      timeKey={TIME_KEY_15}
-                      tournamentSnapshot={tournamentSnapshot}
-                      participants={tournamentData.time15?.participants}
-                    />
-                    <TournamentAtTime
-                      tournamentDateId={selectedDate}
-                      timeKey={TIME_KEY_19}
-                      tournamentSnapshot={tournamentSnapshot}
-                      participants={tournamentData.time19?.participants}
-                    />
-                    <TournamentAtTime
-                      tournamentDateId={selectedDate}
-                      timeKey={TIME_KEY_23}
-                      tournamentSnapshot={tournamentSnapshot}
-                      participants={tournamentData.time23?.participants}
-                    />
-                  </div>
-                </div>
-              );
-            })
-          ) : (
-            <div className={classes.tableInfo}>
-              <h1>Турнир по введенной дате не найден!</h1>
-            </div>
-          )}
-        </div>
-      )}
     </section>
   );
 };
