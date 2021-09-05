88aa630292cfd9d18c532258fb8cf0b757020213
Aluminaze <aluminaze@gmail.com>
2021-09-05 17:25:22 +0300

    update logic to display "no data"

--- src/components/TournamentTable/index.tsx
+++ src/components/TournamentTable/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useList } from "react-firebase-hooks/database";
 import useStyles from "./styles";
 import { TournamentStruct } from "interfacesAndTypes";
@@ -19,66 +19,84 @@ const TournamentTable = (props: TournamentTableProps): JSX.Element | null => {
   const { date } = props;
   const classes = useStyles();
   const database = firebase.database();
-
-  // firebase refs
+  const [tournamentSnapshot, setTournamentSnapshot] = useState<any>(null);
+  const [tournamentData, setTournamentData] = useState<null | TournamentStruct>(
+    null
+  );
+  const [isTableEmpty, setIsTableEmpty] = useState<boolean | null>(null);
   const refTournaments = database.ref("tournaments");
-
-  // firebase data
-  const [tournamentsAtDay, loadingTournamentsAtDay] = useList(
+  const [tournamentsAtDay] = useList(
     refTournaments.orderByChild("id").equalTo(date)
   );
 
-  if (loadingTournamentsAtDay) {
-    return null;
-  }
-
-  if (date) {
+  useEffect(() => {
     if (tournamentsAtDay?.length) {
-      return (
-        <div className={classes.table}>
-          {tournamentsAtDay.map((tournamentSnapshot, index: number) => {
-            const tournamentData: TournamentStruct = tournamentSnapshot.val();
-
-            return (
-              <div className={classes.tableWrapper} key={index}>
-                <div className={classes.tableContainer}>
-                  <TournamentAtTime
-                    tournamentDateId={date}
-                    timeKey={TIME_KEY_11}
-                    tournamentSnapshot={tournamentSnapshot}
-                    participants={tournamentData.time11?.participants}
-                  />
-                  <TournamentAtTime
-                    tournamentDateId={date}
-                    timeKey={TIME_KEY_15}
-                    tournamentSnapshot={tournamentSnapshot}
-                    participants={tournamentData.time15?.participants}
-                  />
-                  <TournamentAtTime
-                    tournamentDateId={date}
-                    timeKey={TIME_KEY_19}
-                    tournamentSnapshot={tournamentSnapshot}
-                    participants={tournamentData.time19?.participants}
-                  />
-                  <TournamentAtTime
-                    tournamentDateId={date}
-                    timeKey={TIME_KEY_23}
-                    tournamentSnapshot={tournamentSnapshot}
-                    participants={tournamentData.time23?.participants}
-                  />
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      );
+      setTournamentSnapshot(tournamentsAtDay[0]);
+      setTournamentData(tournamentsAtDay[0].val());
+    } else {
+      setTournamentSnapshot(null);
+      setTournamentData(null);
     }
+  }, [tournamentsAtDay]);
 
+  useEffect(() => {
+    if (date) {
+      refTournaments
+        .orderByChild("id")
+        .equalTo(date)
+        .get()
+        .then((snapshot) => {
+          if (snapshot.exists()) {
+            setIsTableEmpty(false);
+          } else {
+            setIsTableEmpty(true);
+          }
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [date]);
+
+  if (isTableEmpty) {
     return (
       <div className={classes.info}>
         <h1 className={classes.infoTitle}>
           Турнир по введенной дате не найден!
         </h1>
+      </div>
+    );
+  }
+
+  if (date && tournamentSnapshot && tournamentData) {
+    return (
+      <div className={classes.table}>
+        <div className={classes.tableWrapper}>
+          <div className={classes.tableContainer}>
+            <TournamentAtTime
+              tournamentDateId={date}
+              timeKey={TIME_KEY_11}
+              tournamentSnapshot={tournamentSnapshot}
+              participants={tournamentData.time11?.participants}
+            />
+            <TournamentAtTime
+              tournamentDateId={date}
+              timeKey={TIME_KEY_15}
+              tournamentSnapshot={tournamentSnapshot}
+              participants={tournamentData.time15?.participants}
+            />
+            <TournamentAtTime
+              tournamentDateId={date}
+              timeKey={TIME_KEY_19}
+              tournamentSnapshot={tournamentSnapshot}
+              participants={tournamentData.time19?.participants}
+            />
+            <TournamentAtTime
+              tournamentDateId={date}
+              timeKey={TIME_KEY_23}
+              tournamentSnapshot={tournamentSnapshot}
+              participants={tournamentData.time23?.participants}
+            />
+          </div>
+        </div>
       </div>
     );
   }
