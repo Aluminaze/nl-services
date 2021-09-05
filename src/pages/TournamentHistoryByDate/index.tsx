74d5c18e025afd3eaa4b77b030bff0584e0b329f
Aluminaze <aluminaze@gmail.com>
2021-09-05 18:26:56 +0300

    display info if incoming date from path is uncorrect

--- src/pages/TournamentHistoryByDate/index.tsx
+++ src/pages/TournamentHistoryByDate/index.tsx
@@ -4,9 +4,16 @@ import TournamentDatePicker from "components/TournamentDatePicker";
 import { useHistory, useParams } from "react-router";
 import moment from "moment";
 import TournamentTable from "components/TournamentTable";
+import WarningIcon from "@material-ui/icons/Warning";
 
 interface ParamProps {
   date: string; // DD-MM-YYYY
+}
+
+enum DateStatus {
+  CHECKING = "CHECKING",
+  CORRECT = "CORRECT",
+  UNCORRECT = "UNCORRECT",
 }
 
 const TournamentHistoryByDate = (): JSX.Element => {
@@ -17,15 +24,19 @@ const TournamentHistoryByDate = (): JSX.Element => {
 
   const [dateAsDate, setDateAsDate] = useState<Date | null>(null);
   const [dateAsString, setDateAsString] = useState<string | null>(null);
+  const [dateStatus, setDateStatus] = useState<DateStatus>(DateStatus.CHECKING);
 
   //
   // NOTE: Если дата корректна, то сохраняем ее в стейт в формате Date
   //
   useEffect(() => {
-    const momentDate = moment(date, "DD-MM-YYYY");
+    const momentDate = moment(date, "DD-MM-YYYY", true);
 
     if (momentDate.isValid()) {
       setDateAsDate(momentDate.toDate());
+      setDateStatus(DateStatus.CORRECT);
+    } else {
+      setDateStatus(DateStatus.UNCORRECT);
     }
   }, [date]);
 
@@ -53,7 +64,18 @@ const TournamentHistoryByDate = (): JSX.Element => {
     <section className={classes.container}>
       <TournamentDatePicker date={dateAsDate} onChange={handleDateChange} />
 
-      <TournamentTable date={dateAsString} />
+      {dateStatus === DateStatus.CORRECT && (
+        <TournamentTable date={dateAsString} />
+      )}
+      {dateStatus === DateStatus.UNCORRECT && (
+        <div className={classes.info}>
+          <div className={classes.infoHeader}>
+            <WarningIcon fontSize="large" style={{ color: "#f44336" }} />
+            <span className={classes.infoTitle}>Ошибка!</span>
+          </div>
+          <span className={classes.infoText}>Введенная дата некорректна!</span>
+        </div>
+      )}
     </section>
   );
 };
--- src/pages/TournamentHistoryByDate/styles.ts
+++ src/pages/TournamentHistoryByDate/styles.ts
@@ -6,6 +6,25 @@ const useStyles = makeStyles((theme: Theme) => ({
     display: "flex",
     flexDirection: "column",
   },
+  info: {
+    display: "flex",
+    flexDirection: "column",
+    margin: "25px 0 0 0",
+  },
+  infoHeader: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  infoTitle: {
+    fontWeight: 600,
+    fontSize: "1.2rem",
+  },
+  infoText: {
+    fontSize: "1rem",
+    textAlign: "center",
+    margin: "10px 0 0 0",
+  },
 }));
 
 export default useStyles;
